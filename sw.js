83d50d90d2ccb905ac389cc90545fa11506230e0
Nagi-Inaba <derideri0223@gmail.com>
2025-08-23 16:47:06 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,23 +1,22 @@
-// Simple cache for offline
-const CACHE = 'tea-picker-simple-v1';
+const CACHE = 'tea-choice-v1';
 const ASSETS = ['/', './', './index.html', './manifest.webmanifest'];
 
-self.addEventListener('install', (e)=>{
+self.addEventListener('install', e=>{
   e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));
   self.skipWaiting();
 });
-self.addEventListener('activate', (e)=>{
+self.addEventListener('activate', e=>{
   e.waitUntil(caches.keys().then(keys=>Promise.all(keys.map(k=>k===CACHE?null:caches.delete(k)))));
   self.clients.claim();
 });
-self.addEventListener('fetch', (e)=>{
+self.addEventListener('fetch', e=>{
   e.respondWith(
-    caches.match(e.request).then(cached=> cached || fetch(e.request).then(res=>{
+    caches.match(e.request).then(cached=>cached||fetch(e.request).then(res=>{
       if(e.request.method==='GET'){
-        const copy = res.clone();
-        caches.open(CACHE).then(c=>c.put(e.request, copy));
+        const copy=res.clone();
+        caches.open(CACHE).then(c=>c.put(e.request,copy));
       }
       return res;
-    }).catch(()=> cached || new Response('オフラインです', {status:200})) )
+    }).catch(()=>cached||new Response('オフラインです',{status:200})))
   );
 });
